9c25555dd72e136611c078831fae9ec2644d1abe
Ashish <reytum@live.com>
2020-01-06 13:38:16 +0530

    add iOS customisation setup

--- ios/KmSample/AppDelegate.m
+++ ios/KmSample/AppDelegate.m
@@ -10,6 +10,8 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "KmSample-Bridging-Header.h"
+#import "KmSample-Swift.h"
 
 @implementation AppDelegate
 
@@ -27,6 +29,8 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+
+  [KommunicateWrapper.shared application:application didFinishLaunchingWithOptions:launchOptions];
   return YES;
 }
 
